2ae75c148fb6277c33508b78a47a640605de22fb
Michel <fistonkajibwami2@gmail.com>
2023-11-28 11:20:03 +0100

    add doc

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+import './App.css'
+
+function App() {
+ 
+  return (
+    <>
+     <h1>Hello Future</h1>
+    </>
+  )
+}
+
+export default App
---
+++ src/pages/Product.jsx
@@ -0,0 +1,6 @@
+function Products(){
+    return(
+        <h1></h1>
+    )
+}
+export default Products;
